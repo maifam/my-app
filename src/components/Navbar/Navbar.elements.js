254c1e570f369ee13e6526e1f7d4e9af5aea5c65
Mai Pham <maitphm@gmail.com>
2021-07-19 18:48:36 -0400

    NavLinks css

--- src/components/Navbar/Navbar.elements.js
+++ src/components/Navbar/Navbar.elements.js
@@ -88,3 +88,23 @@ export const NavItem = styled.li`
     }
 `;
 
+export const NavLinks = styled(Link)`
+    color: white;
+    display: flex;
+    align-items: center;
+    text-decoration: none; 
+    padding: 0.5rem 1rem;
+    height: 100%;
+
+    @media screen and (max-width: 960px) {
+        text-align: center;
+        padding: 2rem;
+        width: 100%;
+        display: table;
+
+        &:hover {
+            color: #4b59f7;
+            transition: all 0.3s ease;
+        }
+    }
+`;
